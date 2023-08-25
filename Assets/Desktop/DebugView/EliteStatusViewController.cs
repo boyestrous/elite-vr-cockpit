2efe8cc2ce4352b68955f810acc1869aa3b26d68
boyestrous <jpboyes56@gmail.com>
2023-08-25 09:48:58 -0500

    Rewired Debug Panel on Desktop UI
    Debug tab now shows actual data (StatusFlags and GuiFocus) instead of placeholders.
    
    Also removed the ED State GameEvent in favor of simpler generic GameEvent. None of the implementations used the raised parameter, so this was pointless and caused problems in testing.

--- Assets/Desktop/DebugView/EliteStatusViewController.cs
+++ Assets/Desktop/DebugView/EliteStatusViewController.cs
@@ -1,33 +1,87 @@
 using EVRC.Core;
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Text;
 using UnityEngine;
+using UnityEngine.Analytics;
 using UnityEngine.UIElements;
 
 namespace EVRC.Desktop
 {
     public class EliteStatusViewController : MonoBehaviour
     {
-        [SerializeField] UIDocument parentUIDocument;
-        [SerializeField] private EliteDangerousState eliteDangerousState;
+        [SerializeField] public UIDocument parentUIDocument;
+        [SerializeField] public EliteDangerousState eliteDangerousState;
 
 
         private VisualElement root; // the root of the whole UI
         private Label timestamp;
         private Label guiFocus;
         private Label statusFlags;
+        private Label processActive;
+        private Label processName;
+        private Label processId;
+        private Label processDir;
 
         public void OnEnable()
         {
             root = parentUIDocument.rootVisualElement;
             timestamp = root.Q<Label>("timestamp-value");
-            guiFocus = root.Q<Label>("guiFocus-value");
-            statusFlags = root.Q<Label>("statusFlags-value");
+            guiFocus = root.Q<Label>("guifocus-value");
+            statusFlags = root.Q<Label>("statusflags-value");
+            // @todo Uncomment this when Flags2 are added to the EliteDangerousState
+            //statusFlags2 = root.Q<Label>("statusFlags2-value");
+            processActive = root.Q<Label>("process-active-value");
+            processName = root.Q<Label>("process-name-value");
+            processId = root.Q<Label>("process-id-value");
+            processDir = root.Q<Label>("process-directory-value");
 
             if (eliteDangerousState == null) 
             {
                 Debug.LogError($"Elite State Object is missing on object: {this.name}");
             }
+        }
+
+        public void Refresh()
+        {
+            timestamp.text = eliteDangerousState.lastStatusFromFile.timestamp;
+            guiFocus.text = eliteDangerousState.guiFocus.ToString();
+            statusFlags.text = FormatStatusFlagsForHumans<EDStatusFlags>(eliteDangerousState.statusFlags);
+            //statusFlags2.text = FormatStatusFlagsForHumans<EDStatusFlags2>(eliteDangerousState.statusFlags2);
+            processActive.text = eliteDangerousState.running.ToString();
+            processName.text = eliteDangerousState.processName;
+            processId.text = eliteDangerousState.processId.ToString();
+            processDir.text = eliteDangerousState.processDirectory;
+        }
+
+        public string FormatStatusFlagsForHumans<T>(T flags) where T : Enum
+        {
+
+            if (!typeof(T).IsEnum)
+            {
+                throw new ArgumentException("T must be an enum type");
+            }
+
+            StringBuilder combinedFlags = new StringBuilder();
+            bool isFirstFlag = true;
+
+            foreach (T value in Enum.GetValues(typeof(T)))
+            {
+                if (flags.HasFlag(value))
+                {
+                    if (!isFirstFlag)
+                    {
+                        combinedFlags.Append(" | ");
+                    }
+
+                    combinedFlags.Append(value.ToString());
+                    isFirstFlag = false;
+                }
+            }
+
+            return combinedFlags.ToString();
+            
         }
     }
 }
